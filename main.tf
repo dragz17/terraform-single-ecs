e60f23c29dde6e4ea11f7b92a579cdcf4fb88989
Boy Suganda Sinaga <boysuganda7@gmail.com>
2021-08-26 22:26:44 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,31 +9,31 @@ resource "alicloud_vswitch" "vswitch" {
   availability_zone = "ap-southeast-5a"
 }
 
-resource "alicloud_security_group" "group" {
+resource "alicloud_security_group" "default" {
   name        = "tf_test_foo"
   description = "foo"
   vpc_id      = alicloud_vpc.vpc.id
 }
 
-resource "alicloud_instance" "instance" {
-  availability_zone          = "ap-southeast-5a" #Jakarta
-  security_groups            = alicloud_security_group.group.*.id
-  instance_type              = "ecs.t5-lc2m1.nano" # Burstable t5
-  system_disk_category       = "cloud_efficiency"
-  image_id                   = "centos_7_7_x64_20G_alibase_20200426.vhd" #"${data.alicloud_images.ubuntu.images.0.id}"
-  instance_name              = "sample_test"
-  vswitch_id                 = alicloud_vswitch.vswitch.id
-  internet_max_bandwidth_out = 2
+data "alicloud_instance_types" "c2g4" {
+  cpu_core_count = 2
+  memory_size    = 4
 }
 
-data "terraform_remote_state" "network" {
-    backend   = "oss"
-    config    = {
-        bucket = "sre-automation-config"
-        key    = "single-ecs.tfstate"
-        prefix = "terraform/tfstate"
-        region = "ap-southeast-5"
-    }
-    outputs   = {}
-    workspace = "default"
+data "alicloud_images" "default" {
+  name_regex  = "^ubuntu"
+  most_recent = true
+  owners      = "system"
+}
+
+# Create a web server
+resource "alicloud_instance" "web" {
+  image_id              = "${data.alicloud_images.default.images.0.id}"
+  internet_charge_type  = "PayByBandwidth"
+
+  instance_type        = "${data.alicloud_instance_types.c2g4.instance_types.0.id}"
+  system_disk_category = "cloud_efficiency"
+  security_groups      = ["${alicloud_security_group.default.id}"]
+  instance_name        = "web"
+  vswitch_id           = "vsw-abc12345"
 }
